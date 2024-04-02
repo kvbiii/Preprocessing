d621257650e64b68848746e5f4ca82ea1e114dcd
Kuba <kuba.koscian001@gmail.com>
2024-04-02 17:58:20 +0200

    Isolation forest from scratch

--- Data_Cleaning/Outliers/Outliers_detection/Isolation_Forest.py
+++ Data_Cleaning/Outliers/Outliers_detection/Isolation_Forest.py
@@ -4,8 +4,8 @@ path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
 from requirements import *
 
-class Random_Forest_Classifier():
-    def __init__(self, threshold=0.05, n_estimators=100, max_samples=2, max_features=None, bootstrap=True, random_state=17):
+class Isolation_Forest():
+    def __init__(self, threshold=0.05, n_estimators=100, max_samples=256, max_features=None, bootstrap=True, random_state=17):
         self.threshold = threshold
         self.check_threshold(threshold=self.threshold)
         self.n_estimators = n_estimators
@@ -46,8 +46,8 @@ class Random_Forest_Classifier():
         self.X_train = self.check_for_object_columns(X=self.X_train)
         self.trees = []
         self.max_depth = math.ceil(np.log2(self.max_samples))
-        for i in range(0, self.n_estimators):
-            X_bootstrap = self.get_bootstrap_data(X=X)
+        for iter in range(0, self.n_estimators):
+            X_bootstrap = self.get_bootstrap_data(X=self.X_train)
             self.tree = self.build_isolation_tree(X=X_bootstrap, depth=0)
             self.trees.append(self.tree)
         self.fit_used = True
@@ -60,7 +60,7 @@ class Random_Forest_Classifier():
             X = X[None, :]
         if(train == False):
             if(self.X_train.shape[1] != X.shape[1]):
-                raise ValueError(f"X has {X.shape[1]} features, but Random_Forest_Classifier is expecting {self.X_train.shape[1]} features as input.")
+                raise ValueError(f"X has {X.shape[1]} features, but Isolation_Forest is expecting {self.X_train.shape[1]} features as input.")
         return X
     
     def check_for_object_columns(self, X):
@@ -79,15 +79,15 @@ class Random_Forest_Classifier():
             return X
     
     def build_isolation_tree(self, X, depth):
-        best_split = self.find_best_split(X=X)
-        if(depth < self.max_depth):
+        if(depth < self.max_depth and len(np.unique(X, axis=0, return_counts=True)[1]) > 1):
+            best_split = self.find_best_split(X=X)
             true_branch = self.build_isolation_tree(X=X[best_split["true_rows"], :], depth=depth+1)
             false_branch = self.build_isolation_tree(X=X[best_split["false_rows"], :], depth=depth+1)
             return Decision_Node(true_branch=true_branch, false_branch=false_branch, feature=best_split["feature"], interpolation_value=best_split["interpolation_value"])
         return Leaf(n = X.shape[0])
 
     def find_best_split(self, X):
-        ratio_best = 0.5
+        ratio_best = 1
         best_split = {"feature": None, "true_rows": None, "false_rows": None, "interpolation_value": None}
         limited_columns = self.get_limited_columns(X=X)
         for feature in limited_columns:
@@ -126,10 +126,63 @@ class Random_Forest_Classifier():
     
     def partition(self, X, feature, interpolation_value):
         return np.where(np.array(X)[:,feature] <= interpolation_value)[0], np.where(np.array(X)[:,feature] > interpolation_value)[0]
+
+    def predict(self, X):
+        self.check_fit(fit_used=self.fit_used)
+        X = self.check_X(X=X, train=False)
+        X = self.check_for_object_columns(X=X)
+        scores = self.anomaly_score(X)
+        prediction = self.compare_with_threshold(anomaly_scores=scores)
+        return prediction
+
+    def score_samples(self, X):
+        self.check_fit(fit_used=self.fit_used)
+        X = self.check_X(X=X, train=False)
+        X = self.check_for_object_columns(X=X)
+        scores = self.anomaly_score(X)
+        return scores
+
+    def find_indices_of_outliers(self, X):
+        scores = self.score_samples(X=X)
+        return np.where(scores >= self.threshold)[0]
+
+    def anomaly_score(self, X):
+        avg_length = self.path_length(X)
+        scores = np.array([np.power(2, -l/self.c(self.X_train.shape[0]))for l in avg_length])
+        return scores
+
+    def path_length(self, X):
+        paths = []
+        for row in X:
+            path = []
+            for tree in self.trees:
+                node = tree
+                length = 0
+                while isinstance(node, Decision_Node):
+                    if row[node.feature] <= node.interpolation_value:
+                        node = node.true_branch
+                    else:
+                        node = node.false_branch
+                    length += 1
+                leaf_size = node.n
+                pathLength = length + self.c(leaf_size)
+                path.append(pathLength)
+            paths.append(path)
+        paths = np.array(paths)
+        return np.mean(paths, axis=1)
+
+    def c(self, n):
+        if(n >= 2):
+            return 2*(np.log(n-1)+0.5772156649)-2*(n-1)/n
+        else:
+            return 0
+
+    def compare_with_threshold(self, anomaly_scores):
+        return np.array([1 if score >= self.threshold else -1 for score in anomaly_scores])
     
     def check_fit(self, fit_used):
         if fit_used == False:
-            raise AttributeError('Random_Forest_Classifier has to be fitted first.')
+            raise AttributeError('Isolation_Forest has to be fitted first.')
 
 class Leaf:
     def __init__(self, n):
