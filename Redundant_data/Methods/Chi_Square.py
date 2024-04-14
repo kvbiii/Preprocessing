b534faa25a8826d4c1f8af7530eeed789447a4ae
Kuba <kuba.koscian001@gmail.com>
2024-04-14 18:50:48 +0200

    Chi Square test from scratch

--- Redundant_data/Methods/Chi_Square.py
+++ Redundant_data/Methods/Chi_Square.py
@@ -1,73 +1,226 @@
-from pathlib import Path
-import sys
-path_root = Path(__file__).parents[2]
-sys.path.append(str(path_root))
-from requirements import *
+import numpy as np
+import pandas as pd
+from scipy.stats import chi2
+import typing
+import warnings
 
-class Chi_Square_Test():
-    def __init__(self):
-        pass
 
-    def check_X(self, X):
-        if not isinstance(X, pd.DataFrame) and not isinstance(X, pd.Series) and not isinstance(X, np.ndarray) and not torch.is_tensor(X):
-            raise TypeError('Wrong type of X. It should be pandas DataFrame, pandas Series, numpy array or torch tensor.')
+class Chi_Square_Test:
+    def check_X(
+        self, X: typing.Union[pd.DataFrame, pd.Series, np.ndarray]
+    ) -> np.ndarray:
+        """
+        Check if X is pandas DataFrame, pandas Series or numpy array and convert it to numpy array.
+
+        Args:
+            X: (Union[pd.DataFrame, pd.Series, np.ndarray]): input data.
+
+        Returns:
+            X: (np.ndarray): converted input data.
+        """
+        if (
+            not isinstance(X, pd.DataFrame)
+            and not isinstance(X, pd.Series)
+            and not isinstance(X, np.ndarray)
+        ):
+            raise TypeError(
+                "Wrong type of X. It should be pandas DataFrame, pandas Series, numpy array."
+            )
         X = np.array(X)
-        if(X.ndim == 1):
+        if X.ndim == 1:
             X = X[None, :]
         return X
-    
-    def check_y(self, y):
-        if not isinstance(y, pd.DataFrame) and not isinstance(y, pd.Series) and not isinstance(y, np.ndarray) and not torch.is_tensor(y):
-            raise TypeError('Wrong type of y. It should be pandas DataFrame, pandas Series, numpy array or torch tensor.')
-        y = np.array(y)
-        if(y.ndim == 1):
-            y = y[None, :]
-        return y
-    
-    def check_assumptions(self, crosstab):
-        if(all(i <= 5 for i in crosstab.flatten())):
-            return False
-        else:
-            return True
 
-    def fit(self, X, y, alpha=0.05):
-        X = self.check_X(X=X)
-        y = self.check_y(y=y)
+    def check_y(
+        self, y: typing.Union[pd.DataFrame, pd.Series, np.ndarray]
+    ) -> np.ndarray:
+        """
+        Check if y is pandas DataFrame, pandas Series or numpy array and convert it to numpy array.
+
+        Args:
+            y: (Union[pd.DataFrame, pd.Series, np.ndarray]): target data.
+
+        Returns:
+            y: (np.ndarray): converted target data.
+        """
+        if (
+            not isinstance(y, pd.DataFrame)
+            and not isinstance(y, pd.Series)
+            and not isinstance(y, np.ndarray)
+        ):
+            raise TypeError(
+                "Wrong type of y. It should be pandas DataFrame, pandas Series, numpy array."
+            )
+        y = np.array(y)
+        if y.ndim != 1:
+            y = y.squeeze()
+        return y
+
+    def check_for_object_columns(self, X: np.ndarray) -> np.ndarray:
+        """
+        Check if X contains object columns and convert it to numeric data.
+
+        Args:
+            X: (np.ndarray): input data.
+
+        Returns:
+            X: (np.ndarray): converted input data.
+        """
+        X = pd.DataFrame(X)
+        if X.select_dtypes(include=np.number).shape[1] != X.shape[1]:
+            raise TypeError(
+                "Your data contains object or string columns. Numeric data is obligated."
+            )
+        return np.array(X)
+
+    def fit(
+        self,
+        X: typing.Union[pd.DataFrame, pd.Series, np.ndarray],
+        y: typing.Union[pd.DataFrame, pd.Series, np.ndarray],
+        alpha: float = 0.05,
+    ):
+        """
+        Perform Chi Square test.
+
+        Args:
+            X: (Union[pd.DataFrame, pd.Series, np.ndarray]): input data.
+            y: (Union[pd.DataFrame, pd.Series, np.ndarray]): target data.
+            alpha: (float): significance level.
+        """
+        X = self.check_X(X)
+        X = self.check_for_object_columns(X)
+        y = self.check_y(y)
         self.crosstab_ = self.crosstab_creation(X=X, y=y)
-        self.assumption_ = self.check_assumptions(crosstab=self.crosstab_)
-        self.test_statistic_ = self.calculate_chi_square_statistic(crosstab=self.crosstab_)
+        self.check_assumptions(crosstab=self.crosstab_)
+        self.test_statistic_ = self.calculate_chi_square_statistic(
+            crosstab=self.crosstab_
+        )
         self.df_ = self.calculate_degrees_of_freedom(crosstab=self.crosstab_)
-        self.p_value_ = self.calculate_p_value(test_statistic=self.test_statistic_, df=self.df_)
+        self.p_value_ = self.calculate_p_value(
+            test_statistic=self.test_statistic_, df=self.df_
+        )
         self.critical_value_ = self.calculate_critical_value(df=self.df_, alpha=alpha)
         self.keep_H0 = self.statistical_inference(p_value=self.p_value_, alpha=alpha)
-        self.V_Cramera_ = self.calculate_V_Cramera(test_statistic=self.test_statistic_, crosstab=self.crosstab_)
-    
-    def crosstab_creation(self, X, y):
+        self.V_Cramera_ = self.calculate_V_Cramera(
+            test_statistic=self.test_statistic_, crosstab=self.crosstab_
+        )
+
+    def crosstab_creation(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """
+        Create crosstab from input data.
+
+        Args:
+            X: (np.ndarray): input data.
+            y: (np.ndarray): target data.
+
+        Returns:
+            crosstab: (np.ndarray): crosstab.
+        """
         return np.array(pd.crosstab(X, y, margins=True))
-    
-    def calculate_chi_square_statistic(self, crosstab):
+
+    def check_assumptions(self, crosstab: np.ndarray) -> bool:
+        """
+        Check if assumptions for Chi Square test are met.
+
+        Args:
+            crosstab: (np.ndarray): crosstab.
+
+        Raises:
+            Warning: if assumptions are not met.
+        """
+        if not all(i <= 5 for i in crosstab.flatten()):
+            self.assumption_ = False
+            warnings.warn("Assumptions for Chi Square test are not met.")
+        else:
+            self.assumption_ = True
+
+    def calculate_chi_square_statistic(self, crosstab: np.ndarray) -> float:
+        """
+        Calculate Chi Square statistic.
+
+        Args:
+            crosstab: (np.ndarray): crosstab.
+
+        Returns:
+            chi_square: (float): Chi Square statistic.
+        """
         chi_square = 0
-        for row in range(0, crosstab.shape[0]-1):
-            for column in range(0, crosstab.shape[1]-1):
+        for row in range(0, crosstab.shape[0] - 1):
+            for column in range(0, crosstab.shape[1] - 1):
                 observed = crosstab[row][column]
-                expected = crosstab[row][-1]*crosstab[-1][column]/crosstab[-1][-1]
-                chi_square += (observed-expected)**2/expected
+                expected = crosstab[row][-1] * crosstab[-1][column] / crosstab[-1][-1]
+                chi_square += (observed - expected) ** 2 / expected
         return chi_square
-    
-    def calculate_degrees_of_freedom(self, crosstab):
-        return (crosstab.shape[0]-2)*(crosstab.shape[1]-2)
-    
-    def calculate_p_value(self, test_statistic, df):
+
+    def calculate_degrees_of_freedom(self, crosstab: np.ndarray) -> int:
+        """
+        Calculate degrees of freedom.
+
+        Args:
+            crosstab: (np.ndarray): crosstab.
+
+        Returns:
+            df: (int): degrees of freedom.
+        """
+        return (crosstab.shape[0] - 2) * (crosstab.shape[1] - 2)
+
+    def calculate_p_value(self, test_statistic: float, df: int) -> float:
+        """
+        Calculate p value.
+
+        Args:
+            test_statistic: (float): test statistic.
+            df: (int): degrees of freedom.
+
+        Returns:
+            p_value: (float): p value.
+        """
         return 1 - chi2.cdf(x=test_statistic, df=df)
-    
-    def calculate_critical_value(self, df, alpha):
+
+    def calculate_critical_value(self, df: int, alpha: float) -> float:
+        """
+        Calculate critical value.
+
+        Args:
+            df: (int): degrees of freedom.
+            alpha: (float): significance level.
+
+        Returns:
+            critical_value: (float): critical value.
+        """
         return chi2.isf(q=alpha, df=df)
-    
-    def statistical_inference(self, p_value, alpha):
-        if(p_value >= alpha):
+
+    def statistical_inference(self, p_value: float, alpha: float) -> bool:
+        """
+        Perform statistical inference.
+
+        Args:
+            p_value: (float): p value.
+            alpha: (float): significance level.
+
+        Returns:
+            bool: (bool): True if H0 is not rejected, False otherwise.
+        """
+        if p_value >= alpha:
             return True
         else:
             return False
-    
-    def calculate_V_Cramera(self, test_statistic, crosstab):
-        return (test_statistic/(crosstab[-1][-1]*np.min([crosstab.shape[0]-2, crosstab.shape[1]-2])))**0.5
+
+    def calculate_V_Cramera(self, test_statistic: float, crosstab: np.ndarray) -> float:
+        """
+        Calculate V Cramer test statistic.
+
+        Args:
+            test_statistic: (float): test statistic.
+            crosstab: (np.ndarray): crosstab.
+
+        Returns:
+            V_Cramer: (float): V Cramer test statistic.
+        """
+        return (
+            test_statistic
+            / (
+                crosstab[-1][-1]
+                * np.min([crosstab.shape[0] - 2, crosstab.shape[1] - 2])
+            )
+        ) ** 0.5
