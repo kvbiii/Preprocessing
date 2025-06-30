e034dd18eb91b98419a241715dc6d8fc38048520
Kuba <kuba.koscian001@gmail.com>
2025-06-30 22:01:04 +0200

    Add VIF

--- Redundant_data/Methods/VIF.py
+++ Redundant_data/Methods/VIF.py
@@ -1,38 +1,100 @@
-from pathlib import Path
-import sys
-path_root = Path(__file__).parents[2]
-sys.path.append(str(path_root))
-from requirements import *
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import typing
 
-class VIF():
-    def __init__(self, estimator):
-        self.estimator = estimator
 
-    def check_X(self, X):
-        if not isinstance(X, pd.DataFrame) and not isinstance(X, pd.Series) and not isinstance(X, np.ndarray) and not torch.is_tensor(X):
-            raise TypeError('Wrong type of X. It should be pandas DataFrame, pandas Series, numpy array or torch tensor.')
-        self.features_names = X.columns.tolist()
-        X = np.array(X)
-        if(X.ndim == 1):
-            X = X[None, :]
-        return X
+class VIF:
+    def check_data(
+        self, data: typing.Union[pd.DataFrame, pd.Series, np.ndarray]
+    ) -> np.ndarray:
+        """
+        Check if the data is in the right format.
 
-    def fit(self, X):
-        X = self.check_X(X=X)
+        Args:
+            data (Union[pd.DataFrame, pd.Series, np.ndarray]): data to be checked.
+
+        Returns:
+            data (np.ndarray): one dimensional numpy array data.
+        """
+        if (
+            not isinstance(data, pd.DataFrame)
+            and not isinstance(data, pd.Series)
+            and not isinstance(data, np.ndarray)
+        ):
+            raise TypeError(
+                "Wrong type of data. It should be pandas DataFrame, pandas Series or numpy array."
+            )
+        self.features_names = data.columns.tolist()
+        data = np.array(data)
+        if data.ndim == 2:
+            data = data.squeeze()
+        return data
+
+    def check_for_object_columns(self, data: np.ndarray) -> np.ndarray:
+        """
+        Check if the data contains object columns.
+
+        Args:
+            data (np.ndarray): data to be checked.
+
+        Returns:
+            data (np.ndarray): data after checking for object columns.
+        """
+        data = pd.DataFrame(data)
+        if data.select_dtypes(include=np.number).shape[1] != data.shape[1]:
+            raise TypeError(
+                "Your data contains object or string columns. Numeric data is obligated."
+            )
+        return np.array(data)
+
+    def fit(self, data: typing.Union[pd.DataFrame, pd.Series, np.ndarray]) -> None:
+        """
+        Perform VIF test.
+
+        Args:
+            data (Union[pd.DataFrame, pd.Series, np.ndarray]): data to be tested.
+        """
+        data = self.check_data(data=data)
+        data = self.check_for_object_columns(data=data)
         self.r_squared_ = []
-        for feature in range(0, X.shape[1]):
-            self.r_squared_.append(self.calculate_r_squared(X=X, feature=feature))
+        for feature in range(0, data.shape[1]):
+            self.r_squared_.append(self.calculate_r_squared(X=data, feature=feature))
         self.vif_statistic = self.calculate_vif_statistic(r_squared=self.r_squared_)
-        self.summary_ = pd.DataFrame({'Feature name': self.features_names, 'VIF': self.vif_statistic})
-    
-    def calculate_r_squared(self, X, feature):
+        self.summary_ = pd.DataFrame(
+            {"Feature name": self.features_names, "VIF": self.vif_statistic}
+        )
+
+    def calculate_r_squared(self, X: np.ndarray, feature: int) -> float:
+        """
+        Calculate R squared for a given feature.
+
+        Args:
+            X (np.ndarray): input data.
+            feature (int): feature index.
+
+        Returns:
+            float: R squared value.
+        """
         y = X[:, feature]
         X = np.delete(X, feature, axis=1)
-        self.estimator.fit(X, y)
-        y_pred = self.estimator.predict(X)
-        RSS = np.sum((y-y_pred)**2)
-        TSS = np.sum((y-np.mean(y))**2)
-        return 1-RSS/TSS
-    
-    def calculate_vif_statistic(self, r_squared):
-        return [1/(1-value) for value in r_squared]
+        estimator = LinearRegression(fit_intercept=True)
+        estimator.fit(X, y)
+        y_pred = estimator.predict(X)
+        RSS = np.sum((y - y_pred) ** 2)
+        TSS = np.sum((y - np.mean(y)) ** 2)
+        return 1 - RSS / TSS
+
+    def calculate_vif_statistic(
+        self, r_squared: typing.List[float]
+    ) -> typing.List[float]:
+        """
+        Calculate VIF statistic.
+
+        Args:
+            r_squared (List[float]): list of R squared values.
+
+        Returns:
+            List[float]: list of VIF values.
+        """
+        return [1 / (1 - value) for value in r_squared]
